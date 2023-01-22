fb59d7c14d4b446f0d5d80eb0d19b8e0b3644e7d
shrikant2753 <shrikantrp.00@gmail.com>
2023-01-22 16:08:29 +0530

    simple Bank Application

--- src/Main.java
+++ src/Main.java
@@ -14,11 +14,13 @@ public class Main {
         System.out.println(user.checkBalance());
 
         //deposit
-        System.out.println(user.deposit(1000));
+        System.out.println("enter amount to deposit : ");
+        double amt = sc.nextDouble();
+        System.out.println(user.deposit(amt));
 
         //withdrawal
         System.out.println("enter amount to be withdrawal and password : ");
-        double amt = sc.nextDouble();
+        amt = sc.nextDouble();
         String pas = sc.next();
         System.out.println(user.withdrawal(amt, pas));
     }
